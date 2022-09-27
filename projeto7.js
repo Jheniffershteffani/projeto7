e6728e3213bab466d88709079353aa362d937b88
Jheniffer Shteffani <jheniffer.aleatorio@gmail.com>
2022-09-27 20:57:07 -0300

    criei um site localhost pelo js chamamando o conteudo do html

--- projeto7.js
+++ projeto7.js
@@ -1,11 +1,21 @@
 const http = require("http");
 const host = 'localhost';
-const port = 8000;
+const port = 8001;
+// esta requerindo a pasta que vai ser citada na frente
+const fs = require('fs').promises;
+
+// adicionei a função para chamar o http
+// mostra e recebe o que é feito no navegador
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
+    fs.readFile(__dirname + "/projeto7.html").then(contents => {
+        res.setHeader("Content-Type", "text/html");
+        res.end(contents);
+    })
 };
+//criar um servidor http
 const server = http.createServer(requestListener);
+// inicia o servidor
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+
